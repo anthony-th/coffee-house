d43f453aff428f76c7b0c5dc3eb539fda93de75b
anthony-th <yefremov.anthony@gmail.com>
2024-08-24 05:07:20 +0300

    fix: cursor pointer burger-menu, href change to no_scroll

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,13 @@
 import './styles/main.scss';
 
-import { header, menu, menuLink, itemLink1, itemLink2, itemLink3 } from './components/header';
+import { header, menu, itemLink1, itemLink2, itemLink3 } from './components/header';
 import { menuMain } from './components/menu-main';
 import { footer } from './components/footer';
 
 itemLink1.href = './index.html#favorite';
 itemLink2.href = './index.html#about';
 itemLink3.href = './index.html#mobileapp';
-menuLink.removeAttribute('href');
-menuLink.style.cursor = 'default';
+menu.href = '#no_scroll';
+menu.style.cursor = 'default';
 menu.classList.add('link-active');
 document.body.append(header, menuMain, footer);
